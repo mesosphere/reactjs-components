ca37507057f8920d078f203fc56cf3070defc8c7
John Furrow <johnf@mesosphere.io>
2015-07-16 15:25:37 -0700

    Add key to iterated objects

--- src/List/List.js
+++ src/List/List.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var classNames = require('classnames');
+var _ = require('underscore');
 
 var ListItem = require('./ListItem');
 var ListItemGroup = require('./ListItemGroup');
@@ -7,26 +8,35 @@ var ListItemGroup = require('./ListItemGroup');
 var List = React.createClass({
   displayName: 'List',
 
+  propTypes: {
+    className: React.PropTypes.string,
+    items: React.PropTypes.array.isRequired,
+    tag: React.PropTypes.string
+  },
+
   getDefaultProps: function() {
     return {
       className: ''
     }
   },
 
-  getListItems: function(list) {
+  getListItems: function(list, childIndex) {
     var that = this;
+    var childIndex = childIndex || 0;
 
-    var items = list.map(function(item) {
+    var items = list.map(function(item, parentIndex) {
+      var key = parentIndex + '.' + childIndex;
+      childIndex++;
 
       if (item.items) {
         return (
-          <ListItemGroup tag={item.tag} attributes={item.attributes}>
-            {that.getListItems(item.items)}
+          <ListItemGroup key={key} tag={item.tag} attributes={item.attributes}>
+            {that.getListItems(item.items, childIndex)}
           </ListItemGroup>
         );
       } else {
         return (
-          <ListItem tag={item.tag} attributes={item.attributes}>
+          <ListItem key={key} tag={item.tag} attributes={item.attributes}>
             {item.value}
           </ListItem>
         );
