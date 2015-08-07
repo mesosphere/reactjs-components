c35c005ffb43bc8d1d5d878456f7bc90a1c55f56
Kenny Tran <kenny8tran@gmail.com>
2015-08-07 13:45:36 -0700

    Add more tests for Modal

--- src/Modal/__tests__/Modal-test.js
+++ src/Modal/__tests__/Modal-test.js
@@ -8,13 +8,30 @@ var Modal = require('../Modal');
 
 describe('Modal', function () {
 
+  describe('#getModal', function () {
+    it('should return null if modal is not open', function () {
+      var instance = TestUtils.renderIntoDocument(
+        <Modal open={false} />
+      );
+
+      expect(instance.getModal()).toEqual(null);
+    });
+
+    it('should return an element if modal is open', function () {
+      var instance = TestUtils.renderIntoDocument(
+        <Modal open={true} />
+      );
+
+      var modal = instance.getModal();
+      expect(TestUtils.isElement(modal)).toEqual(true);
+    });
+  });
+
   describe('#onClose', function () {
     beforeEach(function () {
       this.onClose = jasmine.createSpy();
       this.instance = TestUtils.renderIntoDocument(
-        <Modal onClose={this.onClose} open={true}>
-          {this.content}
-        </Modal>
+        <Modal onClose={this.onClose} open={true} />
       );
     });
 
@@ -35,13 +52,52 @@ describe('Modal', function () {
 
       it('should not call onClose if closeByBackdropClick is false', function () {
         var instance = TestUtils.renderIntoDocument(
-          <Modal onClose={this.onClose} open={true} closeByBackdropClick={false}>
-            {this.content}
-          </Modal>
+          <Modal onClose={this.onClose} 
+            open={true} 
+            closeByBackdropClick={false} />
         );
+
         instance.handleBackdropClick();
         expect(this.onClose).not.toHaveBeenCalled();
       });
+    });
+  });
+
+  describe('#getFooter', function () {
+    it('should not return a footer if disabled', function () {
+      var instance = TestUtils.renderIntoDocument(
+        <Modal showFooter={false} />
+      );
+
+      expect(instance.getFooter()).toEqual(null);
+    });
+
+    it('should return a footer if enabled', function () {
+      var instance = TestUtils.renderIntoDocument(
+        <Modal showFooter={true} />
+      );
+
+      var footer = instance.getFooter();
+      expect(TestUtils.isElement(footer)).toEqual(true);
+    });
+  });
+
+  describe('#getCloseButton', function () {
+    it('should not return a button if disabled', function () {
+      var instance = TestUtils.renderIntoDocument(
+        <Modal showCloseButton={false} />
+      );
+
+      expect(instance.getCloseButton()).toEqual(null);
+    });
+
+    it('should return a button if enabled', function () {
+      var instance = TestUtils.renderIntoDocument(
+        <Modal showCloseButton={true} />
+      );
+
+      var closeButton = instance.getCloseButton();
+      expect(TestUtils.isElement(closeButton)).toEqual(true);
     });
   });
 });
