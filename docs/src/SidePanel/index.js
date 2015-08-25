4fc663ec2c50daebb4ecb145e13dc71657817415
mlunoe <michael.lunoe@gmail.com>
2015-08-25 13:14:29 -0700

    Adding some more examples

--- docs/src/SidePanel/index.js
+++ docs/src/SidePanel/index.js
@@ -26,16 +26,24 @@ class SidePanelExample extends React.Component {
     this.setState(state);
   }
 
-  getHeader() {
+  alertAndClosePanelID(id) {
+    /*eslint-disable no-alert */
+    alert(`You just closed panel number ${id}!`);
+    /*eslint-enable no-alert */
+    this.handlePanelClose(id);
+  }
+
+  getHeader(id, description) {
     return (
       <div>
         <button className="button button-stroke button-rounded"
-          onClick={this.handlePanelClose.bind(this, 1)}>
+          onClick={this.alertAndClosePanelID.bind(this, id)}>
           ✕
         </button>
         <h2 className="side-panel-header-title text-align-center flush-top flush-bottom">
-          Panel #1
+          {`Panel #${id}`}
         </h2>
+        <p className="center">{description}</p>
       </div>
     );
   }
@@ -44,39 +52,70 @@ class SidePanelExample extends React.Component {
     return (
       <div className="row canvas-pod canvas-pod-light">
         <div className="container container-pod">
-          <h2>Here is a slide out panel.</h2>
+          <h2>Here are various side panels.</h2>
           <section className="row canvas-pod">
-            <h4 className="inverse">Long content</h4>
-            <button
-              className="button button-inverse"
-              onClick={this.handlePanelOpen.bind(this, 1)}>
-              {'Open slide panel'}
-            </button>
+            <div className="column-6">
+              <h4 className="inverse">Long content - panel header - alert on close</h4>
+              <p>This side panel will scroll vertically, but not horizontally. Click on backdrop will close the panel and this uses a panel header</p>
+              <button
+                className="button button-inverse"
+                onClick={this.handlePanelOpen.bind(this, 1)}>
+                {'Open side panel'}
+              </button>
+            </div>
+            <div className="column-6">
+              <h4 className="inverse">Short, but wide content - backdrop close off - just content</h4>
+              <p>This side panel will scroll horizontally, but not vertically. It does not use a header, the close by backdrop click is turned off, but you can close it by clicking the close button</p>
+              <button
+                className="button button-inverse"
+                onClick={this.handlePanelOpen.bind(this, 2)}>
+                {'Open side panel'}
+              </button>
+            </div>
           </section>
         </div>
+
         <Portal>
-          <SidePanel open={this.state.panel1Open}
-            onClose={this.handlePanelClose.bind(this, 1)}
-            header={this.getHeader()}
-            closeByBackdropClick={true}
-            size="large">
+          <SidePanel header={this.getHeader(1, 'Long content - panel header - alert on close')}
+            open={this.state.panel1Open}
+            onClose={this.alertAndClosePanelID.bind(this, 1)}>
             <div>
-              <div className="container-pod">
+              <p className="containter-pod">
+                This side panel will scroll vertically, but not horizontally. Click on backdrop will close the panel and this uses a panel header
+              </p>
+              <p className="container-pod">
                 Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
+              </p>
+              <p className="container-pod">
                 Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
+              </p>
+              <p className="container-pod">
                 Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
+              </p>
+              <p className="container-pod">
                 Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
+              </p>
+              <p className="container-pod">
                 Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
+              </p>
             </div>
+          </SidePanel>
+        </Portal>
+
+        <Portal>
+          <SidePanel closeByBackdropClick={false}
+            open={this.state.panel2Open}>
+            <button className="button button-stroke button-rounded"
+              onClick={this.handlePanelClose.bind(this, 2)}>
+              ✕
+            </button>
+            <h2 className="center">Short, but wide content - backdrop close off - just content</h2>
+            <p className="containter-pod">
+              This side panel will scroll horizontally, but not vertically. It does not use a header, the close by backdrop click is turned off, but <b>you can close it by clicking the close button</b>
+            </p>
+            <pre>
+              Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
+            </pre>
           </SidePanel>
         </Portal>
       </div>
