4d3f69309b518d376815f79aeba2596b6ce576d1
Kenny Tran <kenny8tran@gmail.com>
2015-08-26 12:53:00 -0700

    Add release npm command

---
+++ main.js
@@ -0,0 +1,11 @@
+var Dropdown = require('./lib/Dropdown/Dropdown');
+var Modal = require('./lib/Modal/Modal');
+var Table = require('./lib/Table/Table');
+var List = require('./lib/List/List');
+
+module.exports = {
+  Dropdown: Dropdown,
+  Modal: Modal,
+  Table: Table,
+  List: List
+};
