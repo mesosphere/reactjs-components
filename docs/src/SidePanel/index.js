805af9ff09b8c7d77ce448ed0c97eef4767e7f35
Kenny Tran <kenny8tran@gmail.com>
2015-10-15 16:49:28 -0700

    Create docs for side panel

--- docs/src/SidePanel/index.js
+++ docs/src/SidePanel/index.js
@@ -69,53 +69,55 @@ class SidePanelExample extends React.Component {
     return (
       <div className="row canvas-pod">
         <div className="container container-pod">
-          <h2>Here are various side panels.</h2>
-          <section className="row canvas-pod">
-            <div className="column-6">
-              <h4 className="inverse">Long content</h4>
-              <p>
-                This side panel will scroll horizontally, but not vertically.
-              </p>
-              <button
-                className="button button-inverse"
-                onClick={this.handlePanelOpen.bind(this, 1)}>
-                Open side panel
-              </button>
+          <h2 className="short-bottom">Side panels</h2>
+          <p>
+            Create a panel that shows itself based on user-interaction. View full source <a href="https://github.com/mesosphere/reactjs-components/blob/master/docs/src/SidePanel/index.js">here</a>.
+          </p>
+          <div className="example-block flush-bottom">
+            <div className="example-block-content">
+              <section className="row canvas-pod">
+                <div className="column-6">
+                  <p>
+                    The panel will pop out from the side; thus, the side panel.
+                  </p>
+                  <button
+                    className="button button-inverse"
+                    onClick={this.handlePanelOpen.bind(this, 1)}>
+                    Open side panel
+                  </button>
+                </div>
+              </section>
             </div>
-            <div className="column-6">
-              <h4 className="inverse">Short, but wide content</h4>
-              <p>
-                This side panel will scroll horizontally, but not vertically.
-              </p>
-              <button
-                className="button button-inverse"
-                onClick={this.handlePanelOpen.bind(this, 2)}>
-                Open side panel
-              </button>
+            <div className="example-block-footer example-block-footer-codeblock">
+              <pre className="prettyprint linenums flush-bottom">
+{`<SidePanel
+  header={this.getHeader()}
+  open={this.state.panelIsOpen}
+  onClose={this.handlePanelClose.bind(this)}>
+  <div>
+    <p className="container-pod container-pod-short flush-bottom">
+      This side panel will scroll vertically, but not horizontally.
+    </p>
+    <p className="container-pod container-pod-short flush-bottom">
+      Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim ...
+    </p>
+    <p className="container-pod container-pod-short flush-bottom">
+      Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim ...
+    </p>
+    <p className="container-pod container-pod-short flush-bottom">
+      Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim ...
+    </p>
+    <p className="container-pod container-pod-short flush-bottom">
+      Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim ...
+    </p>
+    <p className="container-pod container-pod-short flush-bottom">
+      Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim ...
+    </p>
+  </div>
+</SidePanel>`}
+              </pre>
             </div>
-            <div className="column-6">
-              <h4 className="inverse">Alert on close - no header</h4>
-              <p>
-                This side panel will alert whenever the panel is closed, it does not use the panel header
-              </p>
-              <button
-                className="button button-inverse"
-                onClick={this.handlePanelOpen.bind(this, 3)}>
-                Open side panel
-              </button>
-            </div>
-            <div className="column-6">
-              <h4 className="inverse">Backdrop close off</h4>
-              <p>
-                Only way to close this panel is clicking the close button
-              </p>
-              <button
-                className="button button-inverse"
-                onClick={this.handlePanelOpen.bind(this, 4)}>
-                Open side panel
-              </button>
-            </div>
-          </section>
+          </div>
         </div>
 
         <SidePanel header={this.getHeader(1, 'Long content')}
@@ -136,46 +138,6 @@ class SidePanelExample extends React.Component {
             </p>
             <p>
               Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-            </p>
-            <p className="flush-bottom">
-              Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-            </p>
-          </div>
-        </SidePanel>
-
-        <SidePanel header={this.getHeader(2, 'Short, but wide content')}
-          open={this.state.panel2Open}
-          onClose={this.handlePanelClose.bind(this, 2)}>
-          <div className="container container-fluid container-pod">
-            <p>
-              This side panel will scroll horizontally, but not vertically.
-            </p>
-            <pre className="flush-bottom">
-              Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-            </pre>
-          </div>
-        </SidePanel>
-
-        <SidePanel header={this.getHeader(3, 'Alert on Close')}
-          open={this.state.panel3Open}
-          onClose={this.alertAndClosePanelID.bind(this, 3)}>
-          <div className="container container-fluid container-pod">
-            <p>
-              This side panel will alert whenever the panel is closed
-            </p>
-            <p className="flush-bottom">
-              Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-            </p>
-          </div>
-        </SidePanel>
-
-        <SidePanel header={this.getHeader(4, 'Backdrop click is turned off')}
-          open={this.state.panel4Open}
-          closeByBackdropClick={false}
-          onClose={this.handlePanelClose.bind(this, 4)}>
-          <div className="container container-fluid container-pod">
-            <p>
-              Only way to close this panel is clciking the close button
             </p>
             <p className="flush-bottom">
               Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
