6aee177ec10cb6dcfe58eea08146ebdcf881338b
Kenny Tran <kenny8tran@gmail.com>
2015-10-19 11:24:45 -0700

    Add comments for use of function :speech_balloon:

--- docs/src/Util/Util.js
+++ docs/src/Util/Util.js
@@ -3,6 +3,15 @@ import React from 'react';
 /*eslint-enable no-unused-vars*/
 
 const Util = {
+
+  // Pass in an array of objects.
+  // Ex:
+  // {
+  //   name: 'open',
+  //   type: 'bool',
+  //   default: 'false', (this property is optional. don't use if there is no default)
+  //   description: 'To open or not to open... that is the question.'
+  // }
   getPropTypes(props) {
     return props.map(function (prop, i) {
     let propName = prop.name;
