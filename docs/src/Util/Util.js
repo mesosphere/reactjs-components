0d07c610bc49297c94dd71bd32d4050632541939
Kenny Tran <kenny8tran@gmail.com>
2015-10-19 11:15:14 -0700

    Edit util function from what I’ve learned :books:

--- docs/src/Util/Util.js
+++ docs/src/Util/Util.js
@@ -1,31 +1,32 @@
+/*eslint-disable no-unused-vars*/
 import React from 'react';
+/*eslint-enable no-unused-vars*/
 
 const Util = {
   getPropTypes(props) {
-    return props.map(function (prop) {
-      let propName = prop.name;
-      let propType = prop.type;
-      let propDefault = null;
-      let propDescription = prop.description;
+    return props.map(function (prop, i) {
+    let propName = prop.name;
+    let propType = prop.type;
+    let propDefault = null;
+    let propDescription = prop.description;
 
-      if (prop.hasOwnProperty('default')) {
-        propDefault = (
-          <span>
-            &mdash; default: <code>{prop.default}</code>
-          </span>
-        );
-      }
-
-      return (
-        <p>
-          <code>{propName}</code>
-          &mdash; type: <code>{propType}</code>
-          &mdash; type: <code>{propType}</code>
-          {propDefault}
-          {propDescription}
-        </p>
+    if (prop.hasOwnProperty('default')) {
+      propDefault = (
+        <span>
+          &nbsp; &mdash; default: <code>{prop.default}</code>
+        </span>
       );
-    });
+    }
+
+    return (
+      <p key={i}>
+        <code>{propName}</code>
+        &nbsp; &mdash; type: <code>{propType}</code>
+        {propDefault}
+        &nbsp; &mdash; {propDescription}
+      </p>
+    );
+  });
   }
 };
 
