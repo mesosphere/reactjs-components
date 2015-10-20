2dadd9d9a3b87e74c1c2445bf153879e74f04729
Christine Sun <christine.sun@mesosphere.io>
2015-10-20 16:25:14 -0700

    minor changes to code sample and syntax

--- docs/src/Modal/index.js
+++ docs/src/Modal/index.js
@@ -20,23 +20,19 @@ class ModalExample extends Util.mixin(BindMixin) {
   // In order to use a modal, have an interaction that changes
   // `open` to true.
   handleModalOpen() {
-    let state = {};
-    state.open = true;
-    this.setState(state);
+    this.setState({open: true});
   }
 
   // Pass the modal a function that will allow itself to close, by
   // setting `open` to false.
   handleModalClose() {
-    let state = {};
-    state.open = false;
-    this.setState(state);
+    this.setState({open: false});
   }
 
   getModalFooter() {
     return (
       <div>
-        <h5 className="text-align-center">I am Footer</h5>
+        <h5 className="text-align-center">This is not a Footer</h5>
       </div>
     );
   }
@@ -51,7 +47,7 @@ class ModalExample extends Util.mixin(BindMixin) {
 
   render() {
     return (
-      <div className="row canvas-pod canvas-pod-light">
+      <div className="row canvas-pod">
         <div className="container container-pod">
           <section className="row canvas-pod">
             <div className="column-12">
@@ -64,10 +60,35 @@ class ModalExample extends Util.mixin(BindMixin) {
                 <div className="example-block-content">
                   <div className="row row-flex">
                     <div className="column-12">
+                      <p>Here is a modal with a title and footer.</p>
                       <button className="button button-inverse"
-                        onClick={this.handleModalOpen.bind(this)}>
-                        {"Open Modal"}
+                        onClick={this.handleModalOpen}>
+                        Open Modal
                       </button>
+                      <Modal open={this.state.open}
+                        footer={this.getModalFooter()}
+                        showFooter={true}
+                        onClose={this.handleModalClose}
+                        size="large"
+                        titleText="Modal">
+                        <div>
+                          <div className="container-pod">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl dolor, finibus vel egestas et, scelerisque placerat quam.Etiam purus mauris, tempor vel lorem vitae, finibus semper tortor. Nulla nisi nisl, tempus vitae risus ut, gravida elementum purus.Cras scelerisque quis velit at aliquet. Aenean congue faucibus magna nec pellentesque. Nulla facilisi. Etiam feugiat consequat metus,eget consectetur erat sollicitudin in. Maecenas posuere lorem lorem, eu porttitor leo fermentum at. Phasellus volutpat,neque at faucibus dapibus, odio quam molestie lorem, vel gravida lectus diam sit amet neque. Cras ultricies auctor diam,a varius massa eleifend quis. Nulla nec rhoncus odio
+                          </div>
+                          <div className="container-pod">
+                            Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
+                          </div>
+                          <div className="container-pod">
+                            Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
+                          </div>
+                          <div className="container-pod">
+                            Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
+                          </div>
+                          <div className="container-pod">
+                            Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
+                          </div>
+                        </div>
+                      </Modal>
                     </div>
                   </div>
                 </div>
@@ -76,25 +97,37 @@ class ModalExample extends Util.mixin(BindMixin) {
 
 {`import Modal from 'Modal.js';
 
-[...]
+// In order to use a modal, have an interaction that changes
+// 'open' to true.
+handleModalOpen() {
+  this.setState({open: true});
+}
+
+// Pass the modal a function that will allow itself to close, by
+// setting 'open' to false.
+handleModalClose() {
+  this.setState({open: false});
+}
 
 getModalFooter() {
   return (
     <div>
-      <h5 className="text-align-center">I am Footer</h5>
+      <h5 className="text-align-center">This is not a Footer</h5>
     </div>
   );
 }
 
-[...]
-
 render() {
   return (
+    <button className="button button-inverse"
+      onClick={this.handleModalOpen}>
+      Open Modal
+    </button>
     <Modal
       open={this.state.open}
       footer={this.getModalFooter()}
       showFooter={true}
-      onClose={this.handleModalClose.bind(this)}
+      onClose={this.handleModalClose}
       size="large"
       titleText="Modal">
       <div>
@@ -103,38 +136,12 @@ render() {
     </Modal>
   )
 }
-
-[...]
 `}
                   </pre>
                 </div>
               </div>
             </div>
           </section>
-          <Modal open={this.state.open}
-            footer={this.getModalFooter()}
-            showFooter={true}
-            onClose={this.handleModalClose.bind(this)}
-            size="large"
-            titleText="Modal">
-            <div>
-              <div className="container-pod">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl dolor, finibus vel egestas et, scelerisque placerat quam.Etiam purus mauris, tempor vel lorem vitae, finibus semper tortor. Nulla nisi nisl, tempus vitae risus ut, gravida elementum purus.Cras scelerisque quis velit at aliquet. Aenean congue faucibus magna nec pellentesque. Nulla facilisi. Etiam feugiat consequat metus,eget consectetur erat sollicitudin in. Maecenas posuere lorem lorem, eu porttitor leo fermentum at. Phasellus volutpat,neque at faucibus dapibus, odio quam molestie lorem, vel gravida lectus diam sit amet neque. Cras ultricies auctor diam,a varius massa eleifend quis. Nulla nec rhoncus odio
-              </div>
-              <div className="container-pod">
-                Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
-                Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
-                Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-              <div className="container-pod">
-                Fusce sed nibh luctus, ultricies urna eget, eleifend lectus. Etiam sagittis mauris et enim tristique, dignissim varius sem placeratSed molestie purus vitae hendrerit congue. Aliquam viverra cursus odio a ullamcorper.Curabitur venenatis ex quis volutpat suscipit. Nulla ante purus, laoreet vel fermentum vitae, sollicitudin nec erat.Proin venenatis libero sodales, dictum augue non, pretium dolor
-              </div>
-            </div>
-          </Modal>
         </div>
       </div>
     );
