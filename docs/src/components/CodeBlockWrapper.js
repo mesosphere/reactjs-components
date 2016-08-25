0e03d1be8835180fc88429b6b8757450483c4de5
John Furrow <johnf@mesosphere.io>
2016-08-25 10:44:04 -0700

    Add reusable components for creating our docs

---
+++ docs/src/components/CodeBlockWrapper.js
@@ -0,0 +1,28 @@
+import classnames from 'classnames/dedupe';
+import React from 'react';
+
+class CodeBlockWrapper extends React.Component {
+  render() {
+    let panelClasses = classnames(
+      'panel pod flush-right flush-left flush-top',
+      this.props.panelClassNames
+    );
+
+    return (
+      <div className={panelClasses}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+CodeBlockWrapper.propTypes = {
+  children: React.PropTypes.node.isRequired,
+  panelClassNames: React.PropTypes.oneOfType([
+    React.PropTypes.array,
+    React.PropTypes.object,
+    React.PropTypes.string
+  ])
+};
+
+module.exports = CodeBlockWrapper;
