d1894013291edd3c1cf29e9ab51cd61480040348
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2019-01-25 16:28:37 +0100

    fix: replace DOMRect

--- src/Util/DOMUtil.js
+++ src/Util/DOMUtil.js
@@ -75,7 +75,16 @@ const DOMUtil = {
         left: 0,
         right: 0,
         top: 0,
-        boundingRect: new DOMRect()
+        boundingRect: {
+          bottom: 0,
+          height: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+          width: 0,
+          x: 0,
+          y: 0
+        }
       };
     }
 
