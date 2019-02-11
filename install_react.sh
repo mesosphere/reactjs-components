f41073727e4bc3e07f3023466ed50b5ff7af88da
Daniel Schmidt <dschmidt@mesosphere.com>
2019-02-11 16:44:08 +0100

    chore: add 16.8 as react version

--- install_react.sh
+++ install_react.sh
@@ -2,3 +2,4 @@
 
 if [ "$REACT_VERSION" = "15.6" ]; then npm install react@15.6 react-dom@15.6; fi
 if [ "$REACT_VERSION" = "16.7" ]; then npm install react@16.7 react-dom@16.7; fi
+if [ "$REACT_VERSION" = "16.8" ]; then npm install react@16.8 react-dom@16.8; fi
